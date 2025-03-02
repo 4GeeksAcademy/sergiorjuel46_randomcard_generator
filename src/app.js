f911fef2f95ac234a442fda080c93ca5e46434bf
sergiorjuel <110314668+sergiorjuel@users.noreply.github.com>
2025-03-02 22:27:15 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,44 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//write your code here
+function generarCarta() {
+  
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const simbolos = ["♠", "♥", "♦", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+  let simboloAleatorio = simbolos[Math.floor(Math.random() * simbolos.length)];
+
+  let carta = document.createElement("div");
+  carta.classList.add("carta");
+
+  let esquinaTop = document.createElement("div");
+  esquinaTop.classList.add("esquina", "top-left");
+  esquinaTop.innerText = simboloAleatorio;
+
+  let esquinaAbajo = document.createElement("div")
+  esquinaAbajo.classList.add("esquina", "bottom-right")
+  esquinaAbajo.innerText = simboloAleatorio;
+
+  let centro = document.createElement("div");
+    centro.classList.add("centro");
+    centro.innerText = valorAleatorio;
+  
+  if (simboloAleatorio == "♥" || simboloAleatorio == "♦") {
+    esquinaTop.classList.add("rojo");
+    esquinaAbajo.classList.add("rojo");
+    centro.classList.add("rojo");
+  }
+
+    carta.appendChild(esquinaTop);
+    carta.appendChild(centro);
+    carta.appendChild(esquinaAbajo);
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = ""; 
+    contenedor.appendChild(carta);
+
+}
+generarCarta();
+
